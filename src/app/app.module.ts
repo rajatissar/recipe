1341c1035689164faf7b1aa40f98437767cc273f
Rajat Kumar <rajatissar94@gmail.com>
2020-06-24 11:06:01 +0530

    material UI added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -25,6 +26,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     NgbModule,
     AppRoutingModule
   ],
