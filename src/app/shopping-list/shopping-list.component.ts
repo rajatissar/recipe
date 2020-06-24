a94f000ffd18438e3b45c1b64cab213c650025cf
Rajat Kumar <rajatissar94@gmail.com>
2020-06-24 17:10:08 +0530

    shopping added

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/modals/ingredient.modal';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
+
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient(
+      'Onion',
+      '1'
+    ),
+    new Ingredient(
+      'Tomato',
+      '2'
+    )
+  ];
 
   constructor() { }
 
