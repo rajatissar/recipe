328679c2c0dd7aa37973491d5be7314948412670
Rajat Kumar <rajatissar94@gmail.com>
2020-06-29 10:16:58 +0530

    recipe updated

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Recipe name 1',
+      'Recipe description 1',
+      'https://i.ytimg.com/vi/kSb62MGJSI4/maxresdefault.jpg'
+    ),
+    new Recipe(
+      'Recipe name 2',
+      'Recipe description 2',
+      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2018/09/dessert-main-image-molten-cake.jpg'
+    )
+  ];
 
   constructor() { }
 
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Recipe } from './recipe.model';
-
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
@@ -9,18 +7,6 @@ import { Recipe } from './recipe.model';
 })
 
 export class RecipesComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe(
-      'Recipe name 1',
-      'Recipe description 1',
-      'https://i.ytimg.com/vi/kSb62MGJSI4/maxresdefault.jpg'
-    ),
-    new Recipe(
-      'Recipe name 2',
-      'Recipe description 2',
-      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2018/09/dessert-main-image-molten-cake.jpg'
-    )
-  ];
 
   constructor() { }
 
