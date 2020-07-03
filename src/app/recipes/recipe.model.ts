2068228e38ae9b54f55893e1597e07576fb514d0
Rajat Kumar <rajatissar94@gmail.com>
2020-07-03 11:31:57 +0530

    ingredient added in recipe

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,21 @@
+import { Ingredient } from '../shared/modals/ingredient.modal';
+
 export class Recipe {
  public name: string;
  public description: string;
  public imagePath: string;
+ public ingredients: Ingredient[];
 
- constructor(name: string, description: string, imagePath: string) {
+
+ constructor(
+  name: string,
+  description: string,
+  imagePath: string,
+  ingredients: Ingredient[]
+ ) {
   this.name = name;
   this.description = description;
   this.imagePath = imagePath;
+  this.ingredients = ingredients;
  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -4,6 +4,7 @@ import {
 } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/modals/ingredient.modal';
 
 @Injectable({
   providedIn: 'root'
@@ -16,12 +17,32 @@ export class RecipeService {
     new Recipe(
       'Recipe name 1',
       'Recipe description 1',
-      'https://i.ytimg.com/vi/kSb62MGJSI4/maxresdefault.jpg'
+      'https://i.ytimg.com/vi/kSb62MGJSI4/maxresdefault.jpg',
+      [
+        new Ingredient(
+          'Onion',
+          '1'
+        ),
+        new Ingredient(
+          'Tomato',
+          '2'
+        )
+      ]
     ),
     new Recipe(
       'Recipe name 2',
       'Recipe description 2',
-      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2018/09/dessert-main-image-molten-cake.jpg'
+      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2018/09/dessert-main-image-molten-cake.jpg',
+      [
+        new Ingredient(
+          'sugar',
+          '1'
+        ),
+        new Ingredient(
+          'milk',
+          '2'
+        )
+      ]
     )];
 
   constructor() { }
